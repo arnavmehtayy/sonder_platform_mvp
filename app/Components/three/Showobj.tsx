bcc016f69e1951f8162c94a15773372aa1047f46
Arnav Mehta <arnavmehta@berkeley.edu>
2024-07-03 17:13:23 -0700

    figured out how to add TranformControl and added it to every object. Now need to create a control for it so that its only active when the user sets a control for it

--- app/Components/three/Showobj.tsx
+++ app/Components/three/Showobj.tsx
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 import { useStore, getObjectSelector } from "@/app/store";
 import { shallow } from "zustand/shallow";
-import React, { memo } from "react";
+import React, { memo, useRef, useState, useEffect } from "react";
+import { TransformControls } from "@react-three/drei";
 
 /*
   * This component handles the rendering of a single object in the scene.
@@ -13,8 +14,18 @@ import React, { memo } from "react";
 
 */
 
+
+
 export const Showobj = memo(({ id }: { id: number }) => {
   const obj = useStore(getObjectSelector(id));
+  const object_ref = useRef<THREE.Mesh>(null);
+  const [controlsEnabled, setControlsEnabled] = useState(false); // used to control when the TransformControls are enabled
+
+  useEffect(() => {
+    if (object_ref.current) {
+      setControlsEnabled(true);
+    }
+  }, [object_ref.current]);  // Enable controls when the object has been loaded i.e has a non-null reference
 
   if (!obj) {
     console.log("No object found for id Showobj: ", id);
@@ -22,13 +33,23 @@ export const Showobj = memo(({ id }: { id: number }) => {
   }
 
   return (
-    <mesh
-      position={[obj.position.x, obj.position.y, 0]}
-      rotation={[obj.rotation.x, obj.rotation.y, obj.rotation.z]}
-      scale={[obj.scale.x, obj.scale.y, obj.scale.z]}
-    >
-      <primitive object={obj.geom} attach="geometry" />
-      <meshBasicMaterial color={obj.color} />
-    </mesh>
+    <>
+      <mesh
+        position={[obj.position.x, obj.position.y, 0]}
+        rotation={[obj.rotation.x, obj.rotation.y, obj.rotation.z]}
+        scale={[obj.scale.x, obj.scale.y, obj.scale.z]}
+        ref={object_ref}
+      >
+        <primitive object={obj.geom} attach="geometry" />
+        <meshBasicMaterial color={obj.color} />
+      </mesh>
+      {controlsEnabled && object_ref.current && (
+        <TransformControls
+          onObjectChange={() => console.log("moved " + id.toString())}
+          showZ={false}
+          object={object_ref.current}
+        />
+      )} {/* we are adding object_ref.current to the and condition to keep typescript happy */}
+    </>
   );
 });
