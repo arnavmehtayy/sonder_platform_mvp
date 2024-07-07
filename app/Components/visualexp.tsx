4cfc7349be2644e63f8a96b8ab5cda7a2607bd35
Arnav Mehta <arnavmehta@berkeley.edu>
2024-07-06 22:29:48 -0700

    added grid to the visualexp

--- app/Components/visualexp.tsx
+++ app/Components/visualexp.tsx
@@ -30,14 +30,14 @@ export default function Experience() {
 
   const { gridSize, ...gridConfig } = useControls({
     gridSize: [10.5, 10.5],
-    cellSize: { value: 0.6, min: 0, max: 10, step: 0.1 },
+    cellSize: { value: 1, min: 0, max: 10, step: 0.1 },
     cellThickness: { value: 1, min: 0, max: 5, step: 0.1 },
     cellColor: '#6f6f6f',
-    sectionSize: { value: 3.3, min: 0, max: 10, step: 0.1 },
+    sectionSize: { value: 2, min: 0, max: 10, step: 0.1 },
     sectionThickness: { value: 1.5, min: 0, max: 5, step: 0.1 },
     sectionColor: '#9d4b4b',
-    fadeDistance: { value: 25, min: 0, max: 100, step: 1 },
-    fadeStrength: { value: 1, min: 0, max: 1, step: 0.1 },
+    fadeDistance: { value: 100, min: 0, max: 200, step: 1 },
+    fadeStrength: { value: 1, min: 0, max: 2, step: 0.1 },
     followCamera: false,
     infiniteGrid: true
   })
