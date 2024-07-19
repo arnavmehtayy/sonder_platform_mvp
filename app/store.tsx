91a92e7fba8556659588a7486742e7d5fecb8d65
Arnav Mehta <arnavmehta@berkeley.edu>
2024-07-18 21:57:07 -0400

    cleaned up store and made sure that all the methods are just set methods to the 3 fundamental lists in the store everything else is done in the selector methods

--- app/store.tsx
+++ app/store.tsx
@@ -5,11 +5,8 @@ import {
   influencesData,
   controlData,
   canvasData,
-} from "@/classes/init_data";
+} from "@/classes/init_data_reg";
 import { obj } from "@/classes/obj";
-import { geomobj } from "@/classes/geomobj";
-import { TransformObj } from "@/classes/transformObj";
-import { Vector2 } from "three";
 import { Control } from "@/classes/Control";
 import { SelectControl } from "@/classes/SelectControl";
 
@@ -17,9 +14,8 @@ export type State = {
   vizobjs: { [id: number]: obj };
   controls: { [id: number]: Control };
   influences: { [id: number]: Influence<any, any, any>[] }; // Changed to an array of influences
-  setControlValue: (id: number) => (value: number) => void;
   setVizObj: (id: number, new_obj: obj) => void;
-  setControlClick: (control_id: number) => (obj_id: number, mode: "select" | "deselect") => void;
+  setControlClick: (control_id: number, new_obj: Control) => void;
 };
 
 export const useStore = create<State>((set, get) => ({
@@ -63,9 +59,19 @@ export const useStore = create<State>((set, get) => ({
     // console.log("setVizObj", id, new_obj);
   },
 
-  setControlValue: (control_id: number) => (value: number) => {
-    // this can only be used for SliderControl<any>
-    const state = get();
+  
+
+  setControlClick: (control_id: number, new_obj: Control) => {
+    set((state) => { return {controls: { ...state.controls, [control_id]: new_obj },} } )
+  },
+  
+}));
+
+export const getObjectsSelector = (state: State) =>
+  Object.values(state.vizobjs);
+export const setVizObjSelector = (state: State) => state.setVizObj;
+export const setControlValueSelector = (control_id: number) => (state: State) => (value: number) =>
+  {
     const control = state.controls[control_id] as SliderControl<any>;
     if (control && state.vizobjs[control.obj_id]) {
       const obj_id = control.obj_id;
@@ -74,48 +80,7 @@ export const useStore = create<State>((set, get) => ({
       const updatedViz = control.set_attribute(viz, value);
       state.setVizObj(obj_id, updatedViz);
     }
-  },
-
-  // setControlClick2: (control_id: number) => (obj_id: number, mode: "select" | "deselect") => {
-  // },
-
-  setControlClick:
-    (control_id: number) => (obj_id: number, mode: "select" | "deselect") => {
-      const control = get().controls[control_id] as SelectControl
-      if(control.isActive) {
-      switch (mode) {
-        case "select":
-          set((state) => {
-            const updatedState = {
-              controls: {
-                ...state.controls,
-                [control_id]: control.SelectObj(obj_id),
-              },
-            };
-            return updatedState;
-          });
-          break;
-        case "deselect":
-          set((state) => {
-            const updatedState = {
-              controls: {
-                ...state.controls,
-                [control_id]: control.deselectObj(obj_id),
-              },
-            };
-            return updatedState;
-          });
-          break;
-      }
-    }
-    },
-}));
-
-export const getObjectsSelector = (state: State) =>
-  Object.values(state.vizobjs);
-export const setVizObjSelector = (state: State) => state.setVizObj;
-export const setControlValueSelector = (control_id: number) => (state: State) =>
-  state.setControlValue(control_id);
+  }
 export const getObjectSelector = (id: number) => (state: State) =>
   state.vizobjs[id];
 export const getControlSelector = (control_id: number) => (state: State) =>
@@ -130,19 +95,23 @@ export const getInfluenceSelector = (master_id: number) => (state: State) =>
   master_id in state.influences ? state.influences[master_id] : [];
 
 
+
 export const SelectObjectControl = (obj_id: number) => (state: State) => () =>  // may be too expensive redo if necessary
 {
   Object.values(state.controls).forEach((control) => {
     if(control instanceof SelectControl) {
-      state.setControlClick(control.id)(obj_id, "select");
-    }
-  })
-}
+      const updatedState = control.SelectObj(obj_id)
+      state.setControlClick(control.id, updatedState);
+    };
+    })
+  }
+
 
 export const DeSelectObjectControl = (state: State) => (obj_id: number) =>{ // may be too expensive redo if necessary
   Object.values(state.controls).forEach((control) => {
     if(control instanceof SelectControl) {
-      state.setControlClick(control.id)(obj_id, "deselect");
+      const updatedState = control.deselectObj(obj_id)
+      state.setControlClick(control.id, updatedState);
     }
   })
 }
