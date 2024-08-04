8d162cda21c55cfeaa69c6e2f30f5e3497b54a89
Arnav Mehta <arnavmehta@berkeley.edu>
2024-08-04 09:53:58 -0400

    vercel things

--- app/experience/thank-you/[id]/page.tsx
+++ app/experience/thank-you/[id]/page.tsx
@@ -3,18 +3,22 @@
 import React from 'react';
 import { useParams } from 'next/navigation';
 
-export default function Thank_you_page({id}: {id: number}) {
+export default function Thank_you_page() {
     const params = useParams();
+    const id = params.id as string;
+
     return (
         <main>
-            <style>{`body, html { touch-action: auto;
-    overflow-y: scroll;
-    overflow: auto;
-    height: 100%;
-  width: 100%; }`}</style>
+            <style>{`body, html { 
+                touch-action: auto;
+                overflow-y: scroll;
+                overflow: auto;
+                height: 100%;
+                width: 100%; 
+            }`}</style>
 
-            <h1> Thank you! </h1>
-            <h1> {params.id} </h1>
+            <h1>Thank you!</h1>
+            <h1>{id}</h1>
         </main>
     );
 }
